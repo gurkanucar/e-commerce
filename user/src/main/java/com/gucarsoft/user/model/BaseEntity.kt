611528af206868da19115c2553cfc2a3541e1d38
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-07-13 15:09:23 +0300

    entities created

---
+++ user/src/main/java/com/gucarsoft/user/model/BaseEntity.kt
@@ -0,0 +1,9 @@
+package com.gucarsoft.user.model
+
+import java.time.LocalDateTime
+
+
+data class BaseEntity(
+    val createdDateval : LocalDateTime? = null,
+    val createdDate: LocalDateTime? = null
+)
