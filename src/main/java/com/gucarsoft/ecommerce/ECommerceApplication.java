bddf155f69dffb530d7fd7424bc8afd0e5327196
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-07-14 14:09:47 +0300

    EnableJpaRepositories and EntityScan annotation added

--- src/main/java/com/gucarsoft/ecommerce/ECommerceApplication.java
+++ src/main/java/com/gucarsoft/ecommerce/ECommerceApplication.java
@@ -2,9 +2,14 @@ package com.gucarsoft.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import user.repository.UserRepository;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"user.repository"})
+@EntityScan("user.model")
 @ComponentScan(basePackages = {"user"})
 public class ECommerceApplication {
 
